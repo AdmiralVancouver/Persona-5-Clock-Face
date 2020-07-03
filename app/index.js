5d7c07971b79cea093d702e5b217861ba47262d9
Kevin <metrix.28@gmail.com>
2020-07-03 14:28:41 -0400

    - hr and steps are now dynamically spaced - add functions for fetching date images

--- app/index.js
+++ app/index.js
@@ -6,11 +6,22 @@ import { BodyPresenceSensor } from "body-presence";
 import { me as appbit } from "appbit";
 import { today } from "user-activity";
 
-//const hrmLabel = document.getElementById("hrm-label");
 const hrmData = document.getElementById("heartrate_num");
 const stepsData = document.getElementById("steps_num");
+const day_of_week = document.getElementById("day_of_week");
+const month_num = document.getElementById("month_num");
+const day_num = document.getElementById("day_num");
 const sensors = [];
-const hrm = new HeartRateSensor({ frequency: 1 });
+const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+const hrm = new HeartRateSensor();
 
 clock.granularity = "seconds";
 
@@ -27,15 +38,21 @@ if (BodyPresenceSensor) {
   body.start();
 }
 
-if (HeartRateSensor) {
+if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
   hrm.addEventListener("reading", () => {
     hrmData.text = hrm.heartRate;
 
-    // REMOVE
-    //stepsData.text = hrm.heartRate * 123;
+    // REMOVE (FOR TESTING)
+    //stepsData.text = Math.pow(hrm.heartRate, 1);
   });
   sensors.push(hrm);
   hrm.start();
+}
+
+if (appbit.permissions.granted("access_activity")) {
+  clock.addEventListener("tick", () => {
+    stepsData.text = today.adjusted.steps || 0;
+  });
 }
 
 display.addEventListener("change", () => {
@@ -46,5 +63,27 @@ display.addEventListener("change", () => {
 });
 
 clock.ontick = (evt) => {
-    stepsData.text = today.adjusted.steps || 0;
+  let d = evt.date;
+
+  //console.log(d.getDay());
+  setDayText(d.getDay());
+  setMonth(d.getMonth());
+  setDayNum(d.getDate());
 };
+
+function setDayText(day) {
+  day_of_week.image = `day_text/${days[day]}.png`;
+  if (day == 5) {
+    day_of_week.width = 174;
+    day_of_week.height = 81;
+  }
+}
+
+function setMonth(month) {
+  month_num.image = `month_number/${month + 1}.png`;
+}
+
+function setDayNum(num) {
+  day_num.image = `day_number/${num}.png`;
+}
+
